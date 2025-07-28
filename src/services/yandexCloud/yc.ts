0eb560be94cc534c4770f087eb4bbe4b9168be91
Gleb Khokhlov <glebkhokhlov@Glebs-MacBook-Air.local>
2025-07-28 16:48:51 +0300

    change local env

--- src/services/yandexCloud/yc.ts
+++ src/services/yandexCloud/yc.ts
@@ -2,8 +2,10 @@ import AWS from "aws-sdk";
 
 const s3 = new AWS.S3({
   endpoint: "https://storage.yandexcloud.net",
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+  },
   region: process.env.AWS_REGION,
   signatureVersion: "v4",
 });

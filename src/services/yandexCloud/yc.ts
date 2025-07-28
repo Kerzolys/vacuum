ec93053d5300fa37efc39671a8af8119db714bab
Gleb Khokhlov <glebkhokhlov@Glebs-MacBook-Air.local>
2025-07-28 16:16:09 +0300

    added ynadex cloud

---
+++ src/services/yandexCloud/yc.ts
@@ -0,0 +1,13 @@
+import AWS from "aws-sdk";
+
+const s3 = new AWS.S3({
+  endpoint: "https://storage.yandexcloud.net",
+  accessKeyId: process.env.REACT_APP_YC_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_YC_SECRET_ACCESS_KEY,
+  region: "ru-central1",
+  signatureVersion: "v4",
+});
+
+const BUCKET_NAME = "vacuum";
+
+export { s3, BUCKET_NAME };

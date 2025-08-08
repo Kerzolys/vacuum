fa25403a09ff524a3f822feaff900dea944d3693
Gleb Khokhlov <glebkhokhlov@Glebs-MacBook-Air.local>
2025-08-08 10:14:46 +0300

    change cover, add lab info

--- src/components/cover/cover.tsx
+++ src/components/cover/cover.tsx
@@ -1,23 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import coverimage from "../../assets/images/coverimage-5.png";
+import { ButtonUI } from "../ui/button-ui/button-ui";
 
 import styles from "./cover.module.scss";
 
 export const Cover = ({}) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => navigate("/laboratory");
   return (
     <main className={styles.cover}>
       {/* <div className={styles.cover__video_wrapper}> */}
-      {/* <iframe
-          className={styles.cover__video}
-          width="100%" height="100%"
-          src="https://www.youtube.com/embed/x7XEVYoepzA?autoplay=1&mute=1&controls=0&modestbranding=0&rel=0&showinfo=0"
-          allow="autoplay; fullscreen"
-          title="Background Video"
-        /> */}
-      <video className={styles.cover__video} autoPlay loop muted controls={false} width="100%">
+      {/* <video className={styles.cover__video} autoPlay loop muted controls={false} width="100%">
         <source src="/assets/cover_video.mp4" type="video/mp4" />
-      </video>
+      </video> */}
       {/* </div> */}
       <img className={styles.cover__image} src={coverimage} />
+      <h1>Vacuum Quartet Lab</h1>
+      <p>20.09 - 30.10</p>
+      <ButtonUI buttonText="Info and Registration" onClick={handleNavigate} />
     </main>
   );
 };
